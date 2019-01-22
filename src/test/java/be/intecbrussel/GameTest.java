7498cf883b8d10040ed1bc261d69e791fe7f64ab
Overzet <overzet@gmail.com>
2019-01-22 15:32:12 +0100

    Second save

--- src/test/java/be/intecbrussel/GameTest.java
+++ src/test/java/be/intecbrussel/GameTest.java
@@ -6,27 +6,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameTest {
 
+    Game game = new Game();
+
+    public void framesInRollsAndPins(int roll, int pins) {
+        for (int i = 0; i < roll; i++) {
+            game.roll(pins);
+        }
+    }
+
     @Test
     public void GutterTest() {
-        Game game = new Game();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        framesInRollsAndPins(20, 0);
         assertEquals(0, game.score());
     }
 
     @Test
     public void TestAllOnes() {
-        Game game = new Game();
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        framesInRollsAndPins(20, 1);
+
         assertEquals(20, game.score());
     }
 
     @Test
     public void testOneSpare() {
-        Game game = new Game();
         game.roll(5);//first frame
         game.roll(5);//spare
         game.roll(5);//bonus second frame
@@ -34,19 +36,20 @@ class GameTest {
         game.roll(3);//bonus third frame
 
 
-        for (int i = 0; i < 15; i++) {
-            game.roll(0);
-        }
+        framesInRollsAndPins(15, 0);
         assertEquals(23, game.score());
     }
 
     @Test
     public void testOneStrike() {
-        Game game = new Game();
         game.roll(10);//strike
         game.roll(3);
         game.roll(4);
+
+        framesInRollsAndPins(16, 0); //16 rolls  0 pins so this is the 8 frames left = 16 rolls
+
         assertEquals(17, game.score());
     }
+
 
 }
